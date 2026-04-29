4030b9b05945ac20ae6b2b662d1bc9ff9aa1f2f2
Mariana M. Melara <167313774+marian4melara7@users.noreply.github.com>
2026-04-29 20:55:57 -0300

    Implement discount calculation logic

--- n3-clean-design/src/calculadoraVoucher.js
+++ n3-clean-design/src/calculadoraVoucher.js
@@ -1,5 +1,6 @@
 export function calcularDesconto(codigo, valor) {
-  // 🚀 Implemente aqui via TDD
+  if (codigo === 'DESC10') return valor * 0.1;
+  if (codigo === 'DESC20') return valor * 0.2;
+  if (codigo === 'FIXO50') return 50;
   return 0;
 }
-
